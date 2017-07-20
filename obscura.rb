3bced1b8cbbff9ab08171e88e85a711851de7d8f
Sven Dahlstrand <sven@dahlstrand.net>
2017-07-20 02:33:18 +0200

    Update to latest version and add simple test.

--- obscura.rb
+++ obscura.rb
@@ -1,12 +1,16 @@
 class Obscura < Formula
   desc "Salvage your beautiful photographs from a Game Boy Camera save file."
   homepage "https://github.com/svendahlstrand/obscura"
-  url "https://github.com/svendahlstrand/obscura/archive/v0.5.0.tar.gz"
-  sha256 "bceb87413c202c90284e959ef56a7c6f69793128d004bf3c76c910da12290738"
+  url "https://github.com/svendahlstrand/obscura/archive/v0.6.0.tar.gz"
+  sha256 "0ad1e28b12f85936279132fe0da0daaa08ce72bfe72a0854c25517908125b064"
 
   def install
     system "make"
 
     bin.install "bin/obscura"
+  end
+
+  test do
+    assert shell_output("obscura").include?("Usage: "), "Should show usage if no parameter is present."
   end
 end
