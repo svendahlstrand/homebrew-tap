e7b97c96473780eb92da0b89aedd94140cf3b3e5
Sven Dahlstrand <sven@dahlstrand.net>
2017-07-20 18:00:00 +0200

    Update to latest version and add HEAD option.

--- obscura.rb
+++ obscura.rb
@@ -1,8 +1,10 @@
 class Obscura < Formula
   desc "Salvage your beautiful photographs from a Game Boy Camera save file."
   homepage "https://github.com/svendahlstrand/obscura"
-  url "https://github.com/svendahlstrand/obscura/archive/v0.6.0.tar.gz"
-  sha256 "0ad1e28b12f85936279132fe0da0daaa08ce72bfe72a0854c25517908125b064"
+  url "https://github.com/svendahlstrand/obscura/archive/v0.7.0.tar.gz"
+  sha256 "ed487fd50c14201ea3e0dd5937355e0c4e5b880bab0ef4469ea7f93545939cad"
+
+  head "https://github.com/svendahlstrand/obscura.git"
 
   def install
     system "make"
@@ -11,6 +13,6 @@ class Obscura < Formula
   end
 
   test do
-    assert shell_output("obscura").include?("Usage: "), "Should show usage if no parameter is present."
+    assert_match "Usage:", shell_output("#{bin}/obscura"), "Should show usage if no parameter is present."
   end
 end
