de4c01acea065448c8b564e0bed3a403e6d3f7c9
Sven Dahlstrand <sven@dahlstrand.net>
2018-02-24 15:12:54 +0100

    Update data64 to version 0.2.0.

--- data64.rb
+++ data64.rb
@@ -1,8 +1,8 @@
 class Data64 < Formula
   desc "Convert any file to Base64 encoded data URI scheme."
   homepage "https://github.com/svendahlstrand/data64"
-  url "https://github.com/svendahlstrand/data64/archive/v0.1.0.tar.gz"
-  sha256 "b703ceb79f676ffd41589f13cd590b5b37549cd3c5af6be68913442cf410065b"
+  url "https://github.com/svendahlstrand/data64/archive/v0.2.0.tar.gz"
+  sha256 "fdc0a272d1193b3cf4f64d8ad17d2b77f3734b6811e1499213c6edec11a53a85"
 
   def install
     bin.install "bin/data64"
